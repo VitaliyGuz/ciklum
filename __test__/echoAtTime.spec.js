2598426ad1b75c36ee0a93de70a267734262f331
vitaliyguz <vitaliy.guz@gmail.com>
2018-02-19 08:28:34 +0200

    Add test for message

--- __test__/echoAtTime.spec.js
+++ __test__/echoAtTime.spec.js
@@ -1,12 +1,16 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
+const redis = require('redis')
 const app = require('../app')
 const should = chai.should()
+const sinon = require('sinon')
+const assert = chai.assert
+const echoAtTime = require('../echoAtTime.controller')
 
 chai.use(chaiHttp)
 
 describe('/POST echoAtTime', function () {
-    it('it should print a message', function (done) {
+    it('it should return status 200', function (done) {
             let time = new Date()
             time.setMinutes(time.getMinutes() + 1)
             const body = {
@@ -20,6 +24,28 @@ describe('/POST echoAtTime', function () {
                     res.should.have.status(200)
                     done()
                 })
+        }
+    )
+
+    it('it should print a message', function (done) {
+            this.timeout(3000)
+            let time = new Date()
+            time.setSeconds(time.getSeconds() + 2)
+            const body = {
+                message: 'this message appears',
+                time: time,
+            }
+            chai.request(app)
+                .post('/echoAtTime')
+                .send(body)
+                .end(function (err, res) {
+                })
+            let spy = sinon.spy(console, 'log')
+            setTimeout(function () {
+                assert(spy.calledWith('this message appears'))
+                spy.restore()
+                done()
+            }, 1000 * 2)
         }
     )
 
